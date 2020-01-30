b036d645f5c3b8d22ad4203ed9510947e2673743
Gelios <Geliosal39hon@gmail.com>
2020-01-30 21:08:53 +0300

    relise mailer

--- src/js/script.js
+++ src/js/script.js
@@ -75,4 +75,19 @@ $(document).ready(function(){
     validateForms('#consultation-form');
     validateForms('#consultation form');
     validateForms('#order form');
+
+    $('form').submit(function(e) {
+        e.preventDefault();
+        $.ajax({
+            type: "POST",
+            url: "mailer/smart.php",
+            data: $(this).serialize()
+        }).done(function() {
+            $(this).find("input").val("");
+            $('#consultation, #order').fadeOut();
+            $('.overlay, #thanks').fadeIn('slow');
+            $('form').trigger('reset');
+        });
+        return false;
+    });
 });
